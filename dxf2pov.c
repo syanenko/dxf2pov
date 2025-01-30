1780e0e62042c6be6d68337cf232e7eafe5835ef
Sergey Yanenko <syanenko65@gmail.com>
2025-01-31 01:18:35 +0300

    Cosmetics

--- dxf2pov.c
+++ dxf2pov.c
@@ -51,7 +51,7 @@ Usage: dxf2pov infile [outfile]\n");
 
   int face = 0;
   
-  fwrite("#declare Geom = mesh\n{\n", 23, 1, fout);
+  fwrite("#declare Geometry = mesh\n{\n", 23, 1, fout);
   while (fgets(line, LINE_MAX, fin) != NULL)
   {
     len = strlen(line);
